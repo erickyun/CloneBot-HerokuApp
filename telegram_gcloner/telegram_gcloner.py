2b47bcb3977d5188ddf1919e8280490cc62a373b
forbot002 <101519772+forbot002@users.noreply.github.com>
2022-05-24 01:15:35 +0300

    Add files via upload

--- telegram_gcloner/telegram_gcloner.py
+++ telegram_gcloner/telegram_gcloner.py
@@ -119,7 +119,7 @@ def main():
     load_handlers(updater.dispatcher)
 
     updater.start_polling()
-    updater.bot.send_message(chat_id=config.USER_IDS[0], text='Welcome to ⚡️ CloneBot. Let\'s copy some data !')
+    updater.bot.send_message(chat_id=config.USER_IDS[0], text='Telegram gcloner first phone, start')
     updater.idle()
 
 
